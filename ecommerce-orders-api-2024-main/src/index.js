276d89b8f722e549d8ae3a0297c7bfcbaafad731
miguel69645 <miancarrilloal@ittepic.edu.mx>
2024-06-08 13:10:46 -0700

    Delete ecommerce-orders-api-2024-main directory

--- ecommerce-orders-api-2024-main/src/index.js
+++
@@ -1,7 +0,0 @@
-import app from './app';
-import config from './config/config';
-
-// Start the server
-app.listen(app.get('port'));
-console.log(`Server is running on: http://${config.HOST}:${app.get('port')}${config.URL_BASE}`
-);
