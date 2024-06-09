4d40925e828f9239c36587145a1ee54b42ee52d6
miguel69645 <miancarrilloal@ittepic.edu.mx>
2024-06-08 19:14:24 -0700

    Delete dist directory

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-var _app = _interopRequireDefault(require("./app"));
-var _config = _interopRequireDefault(require("./config/config"));
-// Start the server
-_app["default"].listen(_app["default"].get('port'));
-console.log("Server is running on: http://".concat(_config["default"].HOST, ":").concat(_app["default"].get('port')).concat(_config["default"].URL_BASE));
