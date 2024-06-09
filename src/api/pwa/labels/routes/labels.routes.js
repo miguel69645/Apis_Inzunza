edef8d9a786377de9c6d42b75afb2445e7bf0228
miguel69645 <miancarrilloal@ittepic.edu.mx>
2024-06-08 18:25:17 -0700

    Delete src directory

--- src/api/pwa/labels/routes/labels.routes.js
+++
@@ -1,9 +0,0 @@
-import {Router} from "express";
-import * as labelsController from "../controllers/labels.controller";
-
-const router = Router();
-
-router.get('/', labelsController.GetAllLabels);
-router.get('/one', labelsController.GetOneLabels);
-
-export default router;
